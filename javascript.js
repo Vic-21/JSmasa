166784c8a700de12c40e48872b7f3e57eefc781b
Vic <victoriaperdomorivera21@gmail.com>
2020-07-21 17:20:28 -0500

    IMC básico

--- javascript.js
+++ javascript.js
@@ -1,3 +1,9 @@
 let peso, altura; //declaro variables
 peso= prompt ("Ingrese su peso"); //asigno valores
-altura= prompt ("Ingrese su altura");
+altura= prompt ("Ingrese su altura"); //asigno valores
+
+function IMC (peso, altura){
+    let resultado = peso/(altura^2);
+     document.write("Su índice de masa corporal es= " + resultado);
+}
+IMC(peso,altura);
